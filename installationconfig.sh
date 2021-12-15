8606ead8555ccfa9911e8c00f8e298b065cb3dfd
rafi01234 <91274188+rafi01234@users.noreply.github.com>
2021-12-15 06:27:20 -0500

    Update installationconfig.sh

--- installationconfig.sh
+++ installationconfig.sh
@@ -18,7 +18,7 @@ export PATH=$PATH:/home/osboxes/.local/bin
 pip3 install twint
 echo "Installing twint... Please wait."
 
-sudo apt install lolcat figlet
+sudo apt install lolcat figlet -y
 echo "Installing lolcat & figlet... Please wait."
 
 sudo apt install git -y
